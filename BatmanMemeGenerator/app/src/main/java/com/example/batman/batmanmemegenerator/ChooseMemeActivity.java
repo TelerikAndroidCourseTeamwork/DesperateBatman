06339f519689e2e9b7f3ac561a24ef7fe360adf1
Novkirishki <novkirishki94@gmail.com>
2016-01-16 23:24:42 +0200

    Changed images gallery, memes are now in resourses, added some memes for testing purposes

--- BatmanMemeGenerator/app/src/main/java/com/example/batman/batmanmemegenerator/ChooseMemeActivity.java
+++ BatmanMemeGenerator/app/src/main/java/com/example/batman/batmanmemegenerator/ChooseMemeActivity.java
@@ -2,64 +2,103 @@ package com.example.batman.batmanmemegenerator;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import com.parse.FindCallback;
-import com.parse.GetCallback;
-import com.parse.GetDataCallback;
-import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.veinhorn.scrollgalleryview.ScrollGalleryView;
-
-import java.util.List;
+import java.util.ArrayList;
 
 public class ChooseMemeActivity extends AppCompatActivity {
-    private ScrollGalleryView scrollGalleryView;
+
+    private ArrayList<Integer> images;
+    private BitmapFactory.Options options;
+    private ViewPager viewPager;
+    private View btnNext, btnPrev;
+    private FragmentStatePagerAdapter adapter;
+    private LinearLayout thumbnailsContainer;
+    private final static int[] resourceIDs = new int[]{
+            R.drawable.meme1,
+            R.drawable.meme2,
+            R.drawable.meme3,
+            R.drawable.meme4,
+            R.drawable.meme5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_meme);
+        images = new ArrayList<>();
 
-        scrollGalleryView = (ScrollGalleryView) findViewById(R.id.scroll_gallery_view);
-        scrollGalleryView
-                .setThumbnailSize(100)
-                .setZoom(true)
-                .setFragmentManager(getSupportFragmentManager());
+        //find view by id
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        thumbnailsContainer = (LinearLayout) findViewById(R.id.container);
+        btnNext = findViewById(R.id.next);
+        btnPrev = findViewById(R.id.prev);
 
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Meme");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> memes, ParseException e) {
-                if (e == null) {
-                    for (ParseObject meme : memes) {
-                        ParseFile image = (ParseFile) meme.get("image");
-                        image.getDataInBackground(new GetDataCallback() {
-                            @Override
-                            public void done(byte[] data, ParseException e) {
-                                if (e == null) {
-                                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-                                    if (bitmap != null) {
-                                        scrollGalleryView.addImage(bitmap);
-                                    }
-                                }
-                                ;
-                            }
-                        });
-                    }
-                } else {
-                    Toast.makeText(ChooseMemeActivity.this, "Cannot connect to database", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        btnPrev.setOnClickListener(onClickListener(0));
+        btnNext.setOnClickListener(onClickListener(1));
+
+        setImagesData();
+
+        // init viewpager adapter and attach
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(), images);
+        viewPager.setAdapter(adapter);
+
+        inflateThumbnails();
     }
 
-    private Bitmap convertDrawableToBitmap(int image) {
-        return ((BitmapDrawable)getResources().getDrawable(image)).getBitmap();
+    private View.OnClickListener onClickListener(final int i) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (i > 0) {
+                    //next page
+                    if (viewPager.getCurrentItem() < viewPager.getAdapter().getCount() - 1) {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                    }
+                } else {
+                    //previous page
+                    if (viewPager.getCurrentItem() > 0) {
+                        viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+                    }
+                }
+            }
+        };
+    }
+
+    private void setImagesData() {
+        for (int i = 0; i < resourceIDs.length; i++) {
+            images.add(resourceIDs[i]);
+        }
+    }
+
+    private void inflateThumbnails() {
+        for (int i = 0; i < images.size(); i++) {
+            View imageLayout = getLayoutInflater().inflate(R.layout.item_image, null);
+            ImageView imageView = (ImageView) imageLayout.findViewById(R.id.img_thumb);
+            imageView.setOnClickListener(onChagePageClickListener(i));
+            options = new BitmapFactory.Options();
+            options.inSampleSize = 3;
+            options.inDither = false;
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), images.get(i), options );
+            imageView.setImageBitmap(bitmap);
+            //set to image view
+            imageView.setImageBitmap(bitmap);
+            //add imageview
+            thumbnailsContainer.addView(imageLayout);
+        }
+    }
+
+    private View.OnClickListener onChagePageClickListener(final int i) {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(i);
+            }
+        };
     }
 }
